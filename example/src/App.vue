cc5d6bbc4e646e877fb544a772479e61d47a8fae
Aarón J. Montes <aaronmontes_92@yahoo.es>
2022-12-17 19:37:56 -0600

    feat: 🎸 Adding example folder

---
+++ example/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <h1 v-if="isMobile">Mobile Screen</h1>
+    <h1 v-else-if="isTablet">Tablet Screen</h1>
+    <h1 v-else-if="isDesktop">Desktop Screen</h1>
+    <p>Adjust screen to see different messages</p>
+  </div>
+</template>
+
+<script>
+import VueBreakpointMixin from 'vue-breakpoint-mixin';
+
+export default {
+  name: 'App',
+  mixins: [VueBreakpointMixin],
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  width: 100%;
+  height: 100vh;
+  color: #2c3e50;
+  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
